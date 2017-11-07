9f97f90aa310d7f34b1443bde410628eb5a47420
weskitt <weskitt@hotmail.com>
2017-11-07 20:50:05 +0800

    0

--- MemData/SENSOR.h
+++ MemData/SENSOR.h
@@ -5,8 +5,8 @@ class SENSOR
 	int value1; //传感器实时值
 	int value0; //内部平衡值，
 	int local; //位置
-	int type1; //类型 温度 颜色 亮度 波响度 波频率 等一系列被分解后的基本
-	int state; //状态 正趋势 负趋势 稳定//
+	int type1; //类型 温度 颜色 亮度 波响度 波频率 等一系列被分解后的基本  或者  虚拟构造的
+	int state; //描述变化状态 正趋势 负趋势 稳定//
 
 public:
 	void self_Updata();

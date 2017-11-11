bf2879ff5396cc7724092491c7d4887ece411fe6
weskitt <weskitt@hotmail.com>
2017-11-11 14:03:30 +0800

    0

---
+++ MemData/HeadFile.h
@@ -0,0 +1,10 @@
+#pragma once
+
+//传感器位置
+
+//传感器类型
+
+
+const int senLocal =4;
+const int senType  =2;
+const int senCount =8;
--- MemData/WISH.h
+++ MemData/WISH.h
@@ -3,7 +3,8 @@ class WISH
 {
 	bool flag;// 期望达成标签
 
-
+	//位置 类型 数量
+	SENSOR Sensors[senLocal][senType][senCount];
 
 
 public:

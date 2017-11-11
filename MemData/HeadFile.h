080cfe8a9aad2beba834d453762ab889a843d7db
weskitt <weskitt@hotmail.com>
2017-11-11 22:10:11 +0800

    0

--- MemData/HeadFile.h
+++ MemData/HeadFile.h
@@ -1,9 +1,18 @@
 #pragma once
 
 //传感器位置
-
+const int senEye	= 0;	//分析图像
+const int senHear	= 1;	//分析音频
+const int senTaste	= 2;	//分析气味
+const int senSkin	= 3;	//分析温度湿度
 //传感器类型
+//senEye
 
+//senHear
+
+//senTaste
+
+//senSkin
 
 const int senLocal =4;
 const int senType  =2;
--- MemData/WISH.h
+++ MemData/WISH.h
@@ -4,7 +4,7 @@ class WISH
 	bool flag;// 期望达成标签
 
 	//位置 类型 数量
-	SENSOR Sensors[senLocal][senType][senCount];
+	SENSOR Sensors[senLocal][senType][senCount];  //1/24秒获取的元数据
 
 
 public:
